3150a91a67cfea84f006c95659d68f24ed773f51
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-05-31 13:07:13 +0300

    Update serversideclient.h
    Co-Authored-By: komashchenko <22940384+komashchenko@users.noreply.github.com>

--- public/serversideclient.h
+++ public/serversideclient.h
@@ -17,30 +17,7 @@
 #pragma pack(push,1)
 class CServerSideClientBase
 {
-	virtual void unk_00() = 0;
-public:
 	virtual ~CServerSideClientBase() = 0;
-	virtual void Connect(uint32_t a2, const char* szName, int nUserID, INetChannel* pNetChannel, bool bFakePlayer, bool bSplitClient, int a8) = 0;
-private:
-	virtual void unk_03() = 0;
-	virtual void unk_04() = 0;
-	virtual void unk_05() = 0;
-	virtual void unk_06() = 0;
-public:
-	virtual void Disconnect(/* ENetworkDisconnectionReason */ int reason) = 0;
-private:
-	virtual void unk_08() = 0;
-	virtual void unk_09() = 0;
-	virtual void unk_10() = 0;
-	virtual void unk_11() = 0;
-	virtual void unk_12() = 0;
-	virtual void unk_13() = 0;
-	virtual void unk_14() = 0;
-	virtual void unk_15() = 0;
-	virtual void unk_16() = 0;
-public:
-	virtual void ClientPrintf(const char*, ...) = 0;
-	virtual bool IsFakeClient() = 0;
 
 public:
 	INetChannel* GetNetChannel() const { return m_NetChannel; };
@@ -54,29 +31,40 @@ public:
 	bool IsSpawned() const { return m_nSignonState >= SIGNONSTATE_NEW; };
 	bool IsActive() const { return m_nSignonState == SIGNONSTATE_FULL; };
 	bool IsFakeClient() const { return m_bFakePlayer; };
+	bool IsHLTV() const { return m_bIsHLTV; }
 	bool IsFullyAuthenticated() { return m_bFullyAuthenticated; }
+	const netadr_t* GetRemoteAddress() const { return &m_NetAdr0; }
+	void ForceFullUpdate() { m_nForceWaitForTick = -1; }
 
 private:
 	[[maybe_unused]] void* m_pVT1; // INetworkMessageProcessingPreFilter
-	[[maybe_unused]] char pad1[0x40];
+	[[maybe_unused]] char pad1[0x30];
 #ifdef __linux__
 	[[maybe_unused]] char pad2[0x10];
 #endif
-	INetChannel* m_NetChannel; // 80 | 96
-	[[maybe_unused]] char pad3[0x4];
-	int32 m_nSignonState; // 92 | 108
-	[[maybe_unused]] char pad4[0x38];
-	bool m_bFakePlayer; // 152 | 168
-	[[maybe_unused]] char pad5[0x7];
-	CPlayerUserId m_UserID; // 160 | 176
-	[[maybe_unused]] char pad6[0x1];
-	CSteamID m_SteamID; // 163 | 179
-	[[maybe_unused]] char pad7[0x25];
-	CPlayerSlot m_nClientSlot; // 208 | 224
-	CEntityIndex m_nEntityIndex; // 212 | 228
-	CUtlString m_Name; // 216 | 232
-	[[maybe_unused]] char pad8[0x8F2];
-	bool m_bFullyAuthenticated; // 2514 | 2530
+	CUtlString m_Name; // 64 | 80
+	CPlayerSlot m_nClientSlot; // 72 | 88
+	CEntityIndex m_nEntityIndex; // 76 | 92
+	[[maybe_unused]] char pad3[0x8];
+	INetChannel* m_NetChannel; // 88 | 104
+	[[maybe_unused]] char pad4[0x4];
+	int32 m_nSignonState; // 100 | 116
+	[[maybe_unused]] char pad5[0x38];
+	bool m_bFakePlayer; // 160 | 176
+	[[maybe_unused]] char pad6[0x7];
+	CPlayerUserId m_UserID; // 168 | 184
+	[[maybe_unused]] char pad7[0x1];
+	CSteamID m_SteamID; // 171 | 187
+	[[maybe_unused]] char pad8[0x19];
+	netadr_t m_NetAdr0; // 204 | 220
+	[[maybe_unused]] char pad9[0x14];
+	netadr_t m_NetAdr1; // 236 | 252
+	[[maybe_unused]] char pad10[0x22];
+	bool m_bIsHLTV; // 282 | 298
+	[[maybe_unused]] char pad11[0x19];
+	int32 m_nForceWaitForTick; // 308 | 324
+	[[maybe_unused]] char pad12[0x882];
+	bool m_bFullyAuthenticated; // 2490 | 2506
 };
 #pragma pack(pop)
 
