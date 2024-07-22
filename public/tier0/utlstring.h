f3c2944f8d8188829dbf3b03f1145482182d9fcd
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-07-22 07:10:31 +0300

    UtlString: add helper functor objects

--- public/tier0/utlstring.h
+++ public/tier0/utlstring.h
@@ -431,4 +431,15 @@ int CUtlConstStringBase<T>::Compare( const T *rhs ) const
 typedef	CUtlConstStringBase<char>		CUtlConstString;
 typedef	CUtlConstStringBase<wchar_t>	CUtlConstWideString;
 
+//-----------------------------------------------------------------------------
+// Helper functor objects.
+//-----------------------------------------------------------------------------
+
+template < typename T > struct UTLConstStringCaselessStringLessFunctor { bool operator()( const CUtlConstStringBase<T>& a, const char *b ) const { return StringFuncs<T>::CaselessCompare( a.Get(), b ) < 0; } };
+template < typename T > struct UTLConstStringCaselessStringEqualFunctor { bool operator()( const CUtlConstStringBase<T>& a, const char *b ) const { return StringFuncs<T>::CaselessCompare( a.Get(), b ) == 0; } };
+
+// Helper function for CUtlMaps with a CUtlString key
+inline bool UtlStringLessFunc( const CUtlString &lhs, const CUtlString &rhs ) { return V_strcmp( lhs.Get(), rhs.Get() ) < 0; } 
+inline bool UtlStringCaseInsensitiveLessFunc( const CUtlString &lhs, const CUtlString &rhs ) { return V_stricmp( lhs.Get(), rhs.Get() ) < 0; } 
+
 #endif // UTLSTRING_H
