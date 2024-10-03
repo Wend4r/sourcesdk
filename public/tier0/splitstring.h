619d36d07ec3a31bd8d22528bcda5481ce4954f9
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-10-03 06:45:27 +0300

    `CSplitString`: replace `Construct` to `Split`

--- public/tier0/splitstring.h
+++ public/tier0/splitstring.h
@@ -8,17 +8,17 @@
 #include "tier1/utlvector.h"
 
 // <Sergiy> placing it here a few days before Cert to minimize disruption to the rest of codebase
-class CSplitString: public CUtlVector<char*, int, CUtlMemory<char*, int> >
+class CSplitString: public CUtlVector<char*, CUtlMemory<char*, int> >
 {
 public:
 	CSplitString(const char *pString, const char *pSeparator, bool bIncludeSeparators = false)
 	{
-		Construct( pString, &pSeparator, 1, bIncludeSeparators);
+		Split( pString, 0, &pSeparator, 1, bIncludeSeparators);
 	}
 
 	CSplitString(const char *pString, const char **pSeparators, int nSeparators, bool bIncludeSeparators = false)
 	{
-		Construct(pString, pSeparators, nSeparators, bIncludeSeparators);
+		Split(pString, 0, pSeparators, nSeparators, bIncludeSeparators);
 	}
 
 	~CSplitString()
@@ -31,7 +31,7 @@ public:
 	// NOTE: If you want to make Construct() public and implement Purge() here, you'll have to free m_szBuffer there
 	//
 private:
-	DLL_CLASS_IMPORT void Construct(const char *pString, const char **pSeparators, int nSeparators, bool bIncludeSeparators);
+	DLL_CLASS_IMPORT void Split(const char *pString, int stringSize, const char **pSeparators, int nSeparators, bool bIncludeSeparators);
 
 	void PurgeAndDeleteElements()
 	{
