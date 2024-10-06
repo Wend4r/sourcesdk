e11af1f2083265053597e1e23dd5287d8d423ee1
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-10-06 11:27:22 +0300

    Update `CSplitString` container

--- public/tier0/splitstring.h
+++ public/tier0/splitstring.h
@@ -5,10 +5,11 @@
 #pragma once
 #endif
 
+#include "tier0/bufferstring.h"
 #include "tier1/utlvector.h"
 
 // <Sergiy> placing it here a few days before Cert to minimize disruption to the rest of codebase
-class CSplitString: public CUtlVector< char* >
+class CSplitString: public CUtlVector< char * >
 {
 public:
 	CSplitString(const char *pString, const char *pSeparator, bool bIncludeSeparators = false) : m_szBuffer(nullptr)
@@ -21,16 +22,12 @@ public:
 		Split(pString, -1, pSeparators, nSeparators, bIncludeSeparators);
 	}
 
-	void SplitString2( char const *pString, const char **pSeparators, int nSeparators )
-	{
-		V_SplitString2( pString, pSeparators, nSeparators, *this );
-	}
-
 	//
 	// NOTE: If you want to make Construct() public and implement Purge() here, you'll have to free m_szBuffer there
 	//
 private:
 	DLL_CLASS_IMPORT void Split(const char *pString, int stringSize, const char **pSeparators, int nSeparators, bool bIncludeSeparators);
+	DLL_CLASS_IMPORT void SetPacked(const char *pString, const char **pSeparators, bool bIncludeSeparators, int stringSize);
 
 	void PurgeAndDeleteElements()
 	{
@@ -39,6 +36,7 @@ private:
 
 private:
 	char *m_szBuffer; // a copy of original string, with '\0' instead of separators
+	CBufferStringGrowable<8> m_szBuffer2;
 };
 
 #endif //#ifndef SPLITSTRING_H
