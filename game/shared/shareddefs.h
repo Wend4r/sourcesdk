de4ba5fd2a67a6745796ed57cb92578947353910
Wend4r <47463683+Wend4r@users.noreply.github.com>
2025-01-27 13:11:54 +0300

    Update `ObserverMode_t` enumeration

--- game/shared/shareddefs.h
+++ game/shared/shareddefs.h
@@ -11,6 +11,7 @@
 #pragma once
 #endif
 
+#include "basetypes.h"
 #include "bittools.h"
 
 #define TICK_INTERVAL			(gpGlobals->interval_per_tick)
@@ -517,15 +518,14 @@ enum RelativeDamagedDirection_t
 
 
 // Spectator Movement modes
-enum
+enum ObserverMode_t : uint32
 {
-	OBS_MODE_NONE = 0,	// not in spectator mode
-	OBS_MODE_DEATHCAM,	// special mode for death cam animation
-	OBS_MODE_FREEZECAM,	// zooms to a target, and freeze-frames on them
-	OBS_MODE_FIXED,		// view from a fixed camera position
-	OBS_MODE_IN_EYE,	// follow a player in first person view
-	OBS_MODE_CHASE,		// follow a player in third person view
-	OBS_MODE_ROAMING,	// free roaming
+	OBS_MODE_NONE = 0,		// not in spectator mode
+	OBS_MODE_FIXED,			// view from a fixed camera position
+	OBS_MODE_IN_EYE,		// follow a player in first person view
+	OBS_MODE_CHASE,			// follow a player in third person view
+	OBS_MODE_ROAMING,		// free roaming
+	OBS_MODE_DIRECTED,
 
 	NUM_OBSERVER_MODES,
 };
