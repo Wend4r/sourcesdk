892434b3526a6fb7f55f8ca8d7eda5a8d48c7c8c
Wend4r <47463683+Wend4r@users.noreply.github.com>
2025-03-15 04:38:10 +0300

    Remove `CUtlSymbol` interate opertators
    Co-authored-by: GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>

--- public/tier1/utlsymbol.h
+++ public/tier1/utlsymbol.h
@@ -37,9 +37,9 @@ typedef unsigned int UtlSymElm_t;
 #define UTL_INVAL_SYMBOL  ((UtlSymId_t)~0)
 
 #define FOR_EACH_SYMBOL( tableName, iter ) \
-	for ( CUtlSymbol iter = 0; iter < (tableName).GetNumStrings(); iter++ )
+	for ( UtlSymElm_t iter = 0; iter < (tableName).GetNumStrings(); iter++ )
 #define FOR_EACH_SYMBOL_BACK( tableName, iter ) \
-	for ( CUtlSymbol iter = (tableName).GetNumStrings()-1; iter >= 0; iter-- )
+	for ( UtlSymElm_t iter = (tableName).GetNumStrings()-1; iter >= 0; iter-- )
 
 class CUtlSymbol
 {
@@ -51,14 +51,6 @@ public:
 	
 	// operator=
 	CUtlSymbol& operator=( CUtlSymbol const& src ) { m_Id = src.m_Id; return *this; }
-
-	// operator++
-	CUtlSymbol& operator++() { ++m_Id; return *this; }
-	CUtlSymbol operator++(int) { CUtlSymbol oldId = *this; ++m_Id; return oldId; }
-
-	// operator--
-	CUtlSymbol& operator--() { --m_Id; return *this; }
-	CUtlSymbol operator--(int) { CUtlSymbol oldId = *this; --m_Id; return oldId; }
 
 	// operator==
 	bool operator==( CUtlSymbol const& src ) const { return m_Id == src.m_Id; }
