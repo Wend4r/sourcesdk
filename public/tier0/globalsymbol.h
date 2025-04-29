3a86e3182baac160c49e900972a2d584759a55e3
Wend4r <47463683+Wend4r@users.noreply.github.com>
2025-04-29 13:44:13 +0300

    Add `CGlobalSymbol` class

---
+++ public/tier0/globalsymbol.h
@@ -0,0 +1,27 @@
+#ifndef GLOBALSYMBOL_H
+#define GLOBALSYMBOL_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include "platform.h"
+
+class CGlobalSymbol
+{
+public:
+	operator uint64() { return Get(); }
+	uint64 Get() { return m_handle; }
+
+private:
+	uint64 m_handle;
+};
+
+using CGlobalSymbolCaseSensitive = CGlobalSymbol;
+
+PLATFORM_INTERFACE CGlobalSymbol FindGlobalSymbolByHash( uint32 hash );
+PLATFORM_INTERFACE CGlobalSymbol FindGlobalSymbol( const char *str );
+PLATFORM_INTERFACE CGlobalSymbol MakeGlobalSymbol( const char *str );
+PLATFORM_INTERFACE CGlobalSymbol MakeGlobalSymbolCaseSensitive( const char *str );
+
+#endif // GLOBALSYMBOL_H
