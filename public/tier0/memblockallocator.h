Nyano1337/hl2sdk

Update CRawAllocator, CUtlMemoryBlockAllocator & Add CUtlVectorRawAllocator

public/tier1/memblockallocator.h
@@ -18,55 +18,219 @@
 #pragma once
 #endif
 
+#include "tier1/rawallocator.h"
 #include "tier1/utlvector.h"
 
+// AMNOTE: Handle that contains page/subpage indexes to allocated memory within internal storage
+// Stored in the following format: 
+// offset within page of size MEMBLOCK_PAGEOFFSET_BIT
+// page index of size MEMBLOCK_PAGEINDEX_BIT
 typedef unsigned int MemBlockHandle_t;
 
 #define MEMBLOCKHANDLE_INVALID ((MemBlockHandle_t)~0)
 
+#define MEMBLOCK_DEFAULT_PAGESIZE (0x800)
+#define MEMBLOCK_PAGESIZE_SECTION1 (0x10000)
+#define MEMBLOCK_PAGESIZE_SECTION2 (0x80000)
+
+#define MEMBLOCK_PAGEINDEX_BIT (11)
+#define MEMBLOCK_PAGEINDEX_MASK ((1 << MEMBLOCK_PAGEINDEX_BIT) - 1)
+
+#define MEMBLOCK_PAGEOFFSET_BIT (21)
+#define MEMBLOCK_PAGEOFFSET_MASK ((1 << MEMBLOCK_PAGEOFFSET_BIT) - 1)
+
 class CUtlMemoryBlockAllocator
 {
 public:
-	DLL_CLASS_IMPORT	CUtlMemoryBlockAllocator( int nInitPages, unsigned int nPageSize, RawAllocatorType_t eAllocatorType );
-	DLL_CLASS_IMPORT	~CUtlMemoryBlockAllocator( void );
+	CUtlMemoryBlockAllocator( int nInitPages = 0, unsigned int nPageSize = MEMBLOCK_DEFAULT_PAGESIZE ) :
+		m_nPageIndexBits( MEMBLOCK_PAGEINDEX_BIT ),
+		m_nPageIndexMask( MEMBLOCK_PAGEINDEX_MASK ),
+		m_nPageOffsetBits( MEMBLOCK_PAGEOFFSET_BIT ),
+		m_nPageOffsetMask( MEMBLOCK_PAGEOFFSET_MASK ),
+		m_MemPages( 0, nInitPages ),
+		m_nPageSize( nPageSize )
+	{}
 
-	DLL_CLASS_IMPORT	void				RemoveAll( size_t nSize = 0 );
-	DLL_CLASS_IMPORT	void				Purge( void );
-	DLL_CLASS_IMPORT	MemBlockHandle_t	Alloc( unsigned int nSize );
-	DLL_CLASS_IMPORT	MemBlockHandle_t	AllocAndCopy( const char* pBuf, unsigned int nSize );
-	DLL_CLASS_IMPORT	size_t				MemUsage( void ) const;
-	DLL_CLASS_IMPORT	void				SetPageSize( unsigned int nPageSize );
+	~CUtlMemoryBlockAllocator( void )
+	{
+		Purge();
+	}
 
-	void*				GetBlock( MemBlockHandle_t handle ) const;
+	uint32 MaxPages() const { return (1 << m_nPageIndexBits); }
+	uint32 MaxPageSize() const { return (1 << m_nPageOffsetBits); }
+
+	// Clears all memory buffers preserving only nSize bytes, which would be treated like a fresh memory
+	void RemoveAll( size_t nSize = 0 );
+	void Purge( void );
+
+	MemBlockHandle_t Alloc( unsigned int nSize );
+
+	size_t MemUsage( void ) const;
+
+	void SetPageSize( unsigned int nPageSize );
+
+	void* GetBlock( MemBlockHandle_t handle ) const;
 
 private:
-	DLL_CLASS_IMPORT	int					FindPageWithSpace( unsigned int nSpace ) const;
+	MemBlockHandle_t CreateHandle( int page_idx ) const
+	{
+		Assert( page_idx >= 0 && page_idx < m_MemPages.Count() );
+		return (m_MemPages[page_idx].m_nUsedSize & m_nPageOffsetMask) | ((page_idx & m_nPageIndexMask) << m_nPageOffsetBits);
+	}
+
+	int GetPageIdxFromHandle( MemBlockHandle_t handle ) const { return (handle >> m_nPageOffsetBits) & m_nPageIndexMask; }
+	int GetPageOffsetFromHandle( MemBlockHandle_t handle ) const { return handle & m_nPageOffsetMask; }
+
+	int CalcPageSize( int page_idx, int requested_size ) const;
+	int FindPageWithSpace( unsigned int nSize ) const;
 
 	struct MemPage_t
 	{
+		unsigned int MemoryLeft() const { return m_nTotalSize - m_nUsedSize; }
+
 		unsigned int	m_nTotalSize;
 		unsigned int	m_nUsedSize;
-		byte*			m_pMemory;
+		byte			*m_pMemory;
 	};
 
-	typedef CUtlVector<MemPage_t, CUtlMemory_RawAllocator<MemPage_t>> MemPagesVec_t;
+	typedef CUtlVectorRawAllocator<MemPage_t> MemPagesVec_t;
 
-	unsigned int			m_nMaxPagesExp;
+	unsigned int			m_nPageIndexBits;
 	unsigned int			m_nPageIndexMask;
-	unsigned int			m_nPageIndexShift;
-	unsigned int			m_nBlockOffsetMask;
+	unsigned int			m_nPageOffsetBits;
+	unsigned int			m_nPageOffsetMask;
 	MemPagesVec_t			m_MemPages;
 	unsigned int			m_nPageSize;
-	RawAllocatorType_t		m_eRawAllocatorType;
 };
+
+inline void CUtlMemoryBlockAllocator::RemoveAll( size_t nSize )
+{
+	size_t accumulated_total = 0;
+	int removed_at = -1;
+
+	FOR_EACH_VEC( m_MemPages, i )
+	{
+		accumulated_total += m_MemPages[i].m_nTotalSize;
+
+		if(removed_at != -1 || nSize && accumulated_total > nSize)
+		{
+			CRawAllocator::Free( m_MemPages[i].m_pMemory );
+
+			if(removed_at == -1)
+				removed_at = i;
+		}
+		else
+		{
+			m_MemPages[i].m_nUsedSize = 0;
+		}
+	}
+
+	if(removed_at != -1)
+	{
+		m_MemPages.RemoveMultipleFromTail( m_MemPages.Count() - removed_at );
+	}
+}
+
+inline void CUtlMemoryBlockAllocator::Purge( void )
+{
+	FOR_EACH_VEC( m_MemPages, i )
+	{
+		CRawAllocator::Free( m_MemPages[i].m_pMemory );
+	}
+
+	m_MemPages.Purge();
+}
+
+inline MemBlockHandle_t CUtlMemoryBlockAllocator::Alloc( unsigned int nSize )
+{
+	int page_idx = FindPageWithSpace( nSize );
+
+	// Allocate new page since we can't fit in existing ones
+	if(page_idx == -1)
+	{
+		if(m_MemPages.Count() >= MaxPages())
+			return MEMBLOCKHANDLE_INVALID;
+
+		page_idx = m_MemPages.AddToTail();
+		int page_size = CalcPageSize( page_idx, nSize );
+
+		size_t adjusted_size = 0;
+		auto &page = m_MemPages[page_idx];
+
+		page.m_pMemory = (byte *)CRawAllocator::Alloc( page_size, &adjusted_size );
+		page.m_nTotalSize = adjusted_size;
+		page.m_nUsedSize = 0;
+	}
+
+	MemBlockHandle_t handle = CreateHandle( page_idx );
+	m_MemPages[page_idx].m_nUsedSize += nSize;
+
+	return handle;
+}
+
+inline size_t CUtlMemoryBlockAllocator::MemUsage( void ) const
+{
+	size_t mem_usage = 0;
+
+	FOR_EACH_VEC( m_MemPages, i )
+	{
+		mem_usage += m_MemPages[i].m_nTotalSize;
+	}
+
+	return mem_usage;
+}
+
+inline void CUtlMemoryBlockAllocator::SetPageSize( unsigned int nPageSize )
+{
+	if(nPageSize > MEMBLOCK_DEFAULT_PAGESIZE)
+		m_nPageSize = MIN( nPageSize, MaxPageSize() );
+	else
+		m_nPageSize = MEMBLOCK_DEFAULT_PAGESIZE;
+}
+
+inline int CUtlMemoryBlockAllocator::CalcPageSize( int page_idx, int requested_size ) const
+{
+	int page_size = MEMBLOCK_DEFAULT_PAGESIZE;
+	if(page_idx >= 8)
+	{
+		if(page_idx >= 16)
+			page_size = MEMBLOCK_PAGESIZE_SECTION2;
+		else
+			page_size = MEMBLOCK_PAGESIZE_SECTION1;
+	}
+
+	page_size = MAX( page_size, m_nPageSize );
+	return MAX( page_size, requested_size );
+}
+
+inline int CUtlMemoryBlockAllocator::FindPageWithSpace( unsigned int nSize ) const
+{
+	if(m_MemPages.Count() > 0)
+	{
+		if(nSize <= m_MemPages.Tail().MemoryLeft())
+		{
+			return m_MemPages.Count() - 1;
+		}
+		else
+		{
+			FOR_EACH_VEC( m_MemPages, i )
+			{
+				if(nSize <= m_MemPages[i].MemoryLeft())
+					return i;
+			}
+		}
+	}
+
+	return -1;
+}
 
 inline void* CUtlMemoryBlockAllocator::GetBlock( MemBlockHandle_t handle ) const
 {
-	int nPageIndex = handle >> m_nPageIndexShift;
-	int nBlockOffset = handle & m_nBlockOffsetMask;
+	int nPageIndex = GetPageIdxFromHandle( handle );
+	int nPageOffset = GetPageOffsetFromHandle( handle );
 
 	if ( nPageIndex >= 0 && nPageIndex < m_MemPages.Count() )
-		return (void*)&m_MemPages[ nPageIndex ].m_pMemory[ nBlockOffset ];
+		return (void*)&m_MemPages[ nPageIndex ].m_pMemory[ nPageOffset ];
 
 	return NULL;
 }

public/tier1/utlsymbollarge.h
@@ -125,11 +125,11 @@ class CUtlSymbolTableLargeBase
 {
 public:
 	// constructor, destructor
-	CUtlSymbolTableLargeBase( int nGrowSize = 0, int nInitSize = 16, RawAllocatorType_t eAllocatorType = RawAllocator_Standard )
-		:	m_HashTable( 0, eAllocatorType ), 
-			m_MemBlocks( nGrowSize, nInitSize, eAllocatorType ), 
+	CUtlSymbolTableLargeBase( int nGrowSize = 0, int nInitSize = 16 )
+		:	m_HashTable( 0 ), 
+			m_MemBlocks( nGrowSize, nInitSize ), 
 			m_Mutex( "CUtlSymbolTableLargeBase" ), 
-			m_MemBlockAllocator( ( nInitSize > 0 ) ? 8 : 0, PAGE_SIZE, eAllocatorType ), 
+			m_MemBlockAllocator( ( nInitSize > 0 ) ? 8 : 0, PAGE_SIZE ), 
 			m_nElementLimit( INT_MAX - 1 ), 
 			m_bThrowError( true )  { }
 
@@ -226,8 +226,8 @@ private:
 		}
 	};
 
-	typedef CUtlHashtable<UtlSymLargeId_t, empty_t, UtlSymTableLargeHashFunctor, UtlSymTableLargeEqualFunctor, UtlSymTableLargeAltKey, CUtlMemory_RawAllocator<CUtlHashtableEntry<UtlSymLargeId_t, empty_t>>> Hashtable_t;
-	typedef CUtlVector< MemBlockHandle_t, CUtlMemory_RawAllocator<MemBlockHandle_t> > MemBlocksVec_t;
+	typedef CUtlHashtable<UtlSymLargeId_t, empty_t, UtlSymTableLargeHashFunctor, UtlSymTableLargeEqualFunctor, UtlSymTableLargeAltKey> Hashtable_t;
+	typedef CUtlVectorRawAllocator< MemBlockHandle_t > MemBlocksVec_t;
 
 	Hashtable_t					m_HashTable;
 	MemBlocksVec_t				m_MemBlocks;

Wend4r/sourcesdk

public/tier0/memblockallocator.h
@@ -18,55 +18,219 @@
 #pragma once
 #endif
 
+#include "tier0/rawallocator.h"
 #include "tier1/utlvector.h"
 
+// AMNOTE: Handle that contains page/subpage indexes to allocated memory within internal storage
+// Stored in the following format: 
+// offset within page of size MEMBLOCK_PAGEOFFSET_BIT
+// page index of size MEMBLOCK_PAGEINDEX_BIT
 typedef unsigned int MemBlockHandle_t;
 
 #define MEMBLOCKHANDLE_INVALID ((MemBlockHandle_t)~0)
 
+#define MEMBLOCK_DEFAULT_PAGESIZE (0x800)
+#define MEMBLOCK_PAGESIZE_SECTION1 (0x10000)
+#define MEMBLOCK_PAGESIZE_SECTION2 (0x80000)
+
+#define MEMBLOCK_PAGEINDEX_BIT (11)
+#define MEMBLOCK_PAGEINDEX_MASK ((1 << MEMBLOCK_PAGEINDEX_BIT) - 1)
+
+#define MEMBLOCK_PAGEOFFSET_BIT (21)
+#define MEMBLOCK_PAGEOFFSET_MASK ((1 << MEMBLOCK_PAGEOFFSET_BIT) - 1)
+
 class CUtlMemoryBlockAllocator
 {
 public:
-	DLL_CLASS_IMPORT	CUtlMemoryBlockAllocator( int nInitPages, unsigned int nPageSize, RawAllocatorType_t eAllocatorType );
-	DLL_CLASS_IMPORT	~CUtlMemoryBlockAllocator( void );
+	CUtlMemoryBlockAllocator( int nInitPages = 0, unsigned int nPageSize = MEMBLOCK_DEFAULT_PAGESIZE ) :
+		m_nPageIndexBits( MEMBLOCK_PAGEINDEX_BIT ),
+		m_nPageIndexMask( MEMBLOCK_PAGEINDEX_MASK ),
+		m_nPageOffsetBits( MEMBLOCK_PAGEOFFSET_BIT ),
+		m_nPageOffsetMask( MEMBLOCK_PAGEOFFSET_MASK ),
+		m_MemPages( 0, nInitPages ),
+		m_nPageSize( nPageSize )
+	{}
 
-	DLL_CLASS_IMPORT	void				RemoveAll( size_t nSize = 0 );
-	DLL_CLASS_IMPORT	void				Purge( void );
-	DLL_CLASS_IMPORT	MemBlockHandle_t	Alloc( unsigned int nSize );
-	DLL_CLASS_IMPORT	MemBlockHandle_t	AllocAndCopy( const char* pBuf, unsigned int nSize );
-	DLL_CLASS_IMPORT	size_t				MemUsage( void ) const;
-	DLL_CLASS_IMPORT	void				SetPageSize( unsigned int nPageSize );
+	~CUtlMemoryBlockAllocator( void )
+	{
+		Purge();
+	}
 
-	void*				GetBlock( MemBlockHandle_t handle ) const;
+	uint32 MaxPages() const { return (1 << m_nPageIndexBits); }
+	uint32 MaxPageSize() const { return (1 << m_nPageOffsetBits); }
+
+	// Clears all memory buffers preserving only nSize bytes, which would be treated like a fresh memory
+	void RemoveAll( size_t nSize = 0 );
+	void Purge( void );
+
+	MemBlockHandle_t Alloc( unsigned int nSize );
+
+	size_t MemUsage( void ) const;
+
+	void SetPageSize( unsigned int nPageSize );
+
+	void* GetBlock( MemBlockHandle_t handle ) const;
 
 private:
-	DLL_CLASS_IMPORT	int					FindPageWithSpace( unsigned int nSpace ) const;
+	MemBlockHandle_t CreateHandle( int page_idx ) const
+	{
+		Assert( page_idx >= 0 && page_idx < m_MemPages.Count() );
+		return (m_MemPages[page_idx].m_nUsedSize & m_nPageOffsetMask) | ((page_idx & m_nPageIndexMask) << m_nPageOffsetBits);
+	}
+
+	int GetPageIdxFromHandle( MemBlockHandle_t handle ) const { return (handle >> m_nPageOffsetBits) & m_nPageIndexMask; }
+	int GetPageOffsetFromHandle( MemBlockHandle_t handle ) const { return handle & m_nPageOffsetMask; }
+
+	int CalcPageSize( int page_idx, int requested_size ) const;
+	int FindPageWithSpace( unsigned int nSize ) const;
 
 	struct MemPage_t
 	{
+		unsigned int MemoryLeft() const { return m_nTotalSize - m_nUsedSize; }
+
 		unsigned int	m_nTotalSize;
 		unsigned int	m_nUsedSize;
-		byte*			m_pMemory;
+		byte			*m_pMemory;
 	};
 
 	typedef CUtlVector_RawAllocator<MemPage_t> MemPagesVec_t;
 
-	unsigned int			m_nMaxPagesExp;
+	unsigned int			m_nPageIndexBits;
 	unsigned int			m_nPageIndexMask;
-	unsigned int			m_nPageIndexShift;
-	unsigned int			m_nBlockOffsetMask;
+	unsigned int			m_nPageOffsetBits;
+	unsigned int			m_nPageOffsetMask;
 	MemPagesVec_t			m_MemPages;
 	unsigned int			m_nPageSize;
-	RawAllocatorType_t		m_eRawAllocatorType;
 };
+
+inline void CUtlMemoryBlockAllocator::RemoveAll( size_t nSize )
+{
+	size_t accumulated_total = 0;
+	int removed_at = -1;
+
+	FOR_EACH_VEC( m_MemPages, i )
+	{
+		accumulated_total += m_MemPages[i].m_nTotalSize;
+
+		if(removed_at != -1 || nSize && accumulated_total > nSize)
+		{
+			CRawAllocator::Free( m_MemPages[i].m_pMemory );
+
+			if(removed_at == -1)
+				removed_at = i;
+		}
+		else
+		{
+			m_MemPages[i].m_nUsedSize = 0;
+		}
+	}
+
+	if(removed_at != -1)
+	{
+		m_MemPages.RemoveMultipleFromTail( m_MemPages.Count() - removed_at );
+	}
+}
+
+inline void CUtlMemoryBlockAllocator::Purge( void )
+{
+	FOR_EACH_VEC( m_MemPages, i )
+	{
+		CRawAllocator::Free( m_MemPages[i].m_pMemory );
+	}
+
+	m_MemPages.Purge();
+}
+
+inline MemBlockHandle_t CUtlMemoryBlockAllocator::Alloc( unsigned int nSize )
+{
+	int page_idx = FindPageWithSpace( nSize );
+
+	// Allocate new page since we can't fit in existing ones
+	if(page_idx == -1)
+	{
+		if(m_MemPages.Count() >= MaxPages())
+			return MEMBLOCKHANDLE_INVALID;
+
+		page_idx = m_MemPages.AddToTail();
+		int page_size = CalcPageSize( page_idx, nSize );
+
+		size_t adjusted_size = 0;
+		auto &page = m_MemPages[page_idx];
+
+		page.m_pMemory = (byte *)CRawAllocator::Alloc( page_size, &adjusted_size );
+		page.m_nTotalSize = adjusted_size;
+		page.m_nUsedSize = 0;
+	}
+
+	MemBlockHandle_t handle = CreateHandle( page_idx );
+	m_MemPages[page_idx].m_nUsedSize += nSize;
+
+	return handle;
+}
+
+inline size_t CUtlMemoryBlockAllocator::MemUsage( void ) const
+{
+	size_t mem_usage = 0;
+
+	FOR_EACH_VEC( m_MemPages, i )
+	{
+		mem_usage += m_MemPages[i].m_nTotalSize;
+	}
+
+	return mem_usage;
+}
+
+inline void CUtlMemoryBlockAllocator::SetPageSize( unsigned int nPageSize )
+{
+	if(nPageSize > MEMBLOCK_DEFAULT_PAGESIZE)
+		m_nPageSize = MIN( nPageSize, MaxPageSize() );
+	else
+		m_nPageSize = MEMBLOCK_DEFAULT_PAGESIZE;
+}
+
+inline int CUtlMemoryBlockAllocator::CalcPageSize( int page_idx, int requested_size ) const
+{
+	int page_size = MEMBLOCK_DEFAULT_PAGESIZE;
+	if(page_idx >= 8)
+	{
+		if(page_idx >= 16)
+			page_size = MEMBLOCK_PAGESIZE_SECTION2;
+		else
+			page_size = MEMBLOCK_PAGESIZE_SECTION1;
+	}
+
+	page_size = MAX( page_size, m_nPageSize );
+	return MAX( page_size, requested_size );
+}
+
+inline int CUtlMemoryBlockAllocator::FindPageWithSpace( unsigned int nSize ) const
+{
+	if(m_MemPages.Count() > 0)
+	{
+		if(nSize <= m_MemPages.Tail().MemoryLeft())
+		{
+			return m_MemPages.Count() - 1;
+		}
+		else
+		{
+			FOR_EACH_VEC( m_MemPages, i )
+			{
+				if(nSize <= m_MemPages[i].MemoryLeft())
+					return i;
+			}
+		}
+	}
+
+	return -1;
+}
 
 inline void* CUtlMemoryBlockAllocator::GetBlock( MemBlockHandle_t handle ) const
 {
-	int nPageIndex = handle >> m_nPageIndexShift;
-	int nBlockOffset = handle & m_nBlockOffsetMask;
+	int nPageIndex = GetPageIdxFromHandle( handle );
+	int nPageOffset = GetPageOffsetFromHandle( handle );
 
 	if ( nPageIndex >= 0 && nPageIndex < m_MemPages.Count() )
-		return (void*)&m_MemPages[ nPageIndex ].m_pMemory[ nBlockOffset ];
+		return (void*)&m_MemPages[ nPageIndex ].m_pMemory[ nPageOffset ];
 
 	return NULL;
 }

public/tier1/utlsymbollarge.h
@@ -122,11 +122,11 @@ class CUtlSymbolTableLargeBase
 {
 public:
 	// constructor, destructor
-	CUtlSymbolTableLargeBase( int nGrowSize = 0, int nInitSize = 16, RawAllocatorType_t eAllocatorType = RawAllocator_Standard )
-		:	m_HashTable( 0, eAllocatorType ), 
-			m_MemBlocks( nGrowSize, nInitSize, eAllocatorType ), 
+	CUtlSymbolTableLargeBase( int nGrowSize = 0, int nInitSize = 16 )
+		:	m_HashTable( 0 ), 
+			m_MemBlocks( nGrowSize, nInitSize ), 
 			m_Mutex( "CUtlSymbolTableLargeBase" ), 
-			m_MemBlockAllocator( ( nInitSize > 0 ) ? 8 : 0, PAGE_SIZE, eAllocatorType ), 
+			m_MemBlockAllocator( ( nInitSize > 0 ) ? 8 : 0, PAGE_SIZE ), 
 			m_nElementLimit( INT_MAX - 1 ), 
 			m_bThrowError( true )  { }
 
@@ -225,7 +225,7 @@ private:
 		}
 	};
 
-	typedef CUtlHashtable< UtlSymLargeElm_t, empty_t, UtlSymTableLargeHashFunctor, UtlSymTableLargeEqualFunctor, UtlSymTableLargeAltKey, CUtlMemory_RawAllocator<CUtlHashtableEntry<UtlSymLargeElm_t, empty_t>> > Hashtable_t;
+	typedef CUtlHashtable< UtlSymLargeElm_t, empty_t, UtlSymTableLargeHashFunctor, UtlSymTableLargeEqualFunctor, UtlSymTableLargeAltKey > Hashtable_t;
 	typedef CUtlVector_RawAllocator< MemBlockHandle_t, UtlSymLargeElm_t > MemBlocksVec_t;
 
 	Hashtable_t					m_HashTable;