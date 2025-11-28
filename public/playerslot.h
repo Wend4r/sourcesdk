2c93560e42ef1a4676835d230826d79c4e4f764a
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2025-11-28 19:04:30 +0300

    Add few QoL methods to CPlayerSlot

--- public/playerslot.h
+++ public/playerslot.h
@@ -5,6 +5,8 @@
 #pragma once
 #endif
 
+#include "const.h"
+
 #define INVALID_PLAYER_SLOT_INDEX -1
 #define INVALID_PLAYER_SLOT CPlayerSlot( INVALID_PLAYER_SLOT_INDEX )
 
@@ -13,27 +15,20 @@ class CPlayerSlot
 public:
 	CPlayerSlot( int slot = INVALID_PLAYER_SLOT_INDEX ) : m_Data( slot ) {}
 
-	operator int() const { return m_Data; }
-	bool operator==( const CPlayerSlot &other ) const { return other.m_Data == m_Data; }
-	bool operator!=( const CPlayerSlot &other ) const { return other.m_Data != m_Data; }
-
+	void Invalidate() { m_Data = INVALID_PLAYER_SLOT_INDEX; }
+	bool IsValid() const { return m_Data >= 0 && m_Data < ABSOLUTE_PLAYER_LIMIT; }
 	int Get() const { return m_Data; }
+
 	int GetEntityIndex() const { return m_Data + 1; }
 	int GetClientIndex() const { return GetEntityIndex(); }
 
 	static int InvalidIndex() { return INVALID_PLAYER_SLOT; }
-	bool IsValid() const { return m_Data != InvalidIndex(); }
 
-	CPlayerSlot& operator++() {
-		++m_Data;
-		return *this;
-	}
-
-	CPlayerSlot operator++(int) {
-		CPlayerSlot temp = *this;
-		++(*this);
-		return temp;
-	}
+	operator int() const { return m_Data; }
+	bool operator==( const CPlayerSlot &other ) const { return other.m_Data == m_Data; }
+	bool operator!=( const CPlayerSlot &other ) const { return other.m_Data != m_Data; }
+	CPlayerSlot& operator++() { ++m_Data; return *this; }
+	CPlayerSlot operator++( int ) { CPlayerSlot temp = *this; ++m_Data; return temp; }
 
 private:
 	int m_Data;
