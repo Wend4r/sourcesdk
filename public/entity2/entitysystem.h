1587f3fb59c374438e4b7677d667a4dc0aace9e7
Napas <olmironov@proton.me>
2026-01-27 11:16:54 +0200

    update IEntityResourceManifest

--- public/entity2/entitysystem.h
+++ public/entity2/entitysystem.h
@@ -47,6 +47,13 @@ public:
 	virtual void AddResource(const char*) = 0;
 	virtual void AddResource(const char*, void*) = 0;
 	virtual void AddResource(const char*, void*, void*, void*) = 0;
+	virtual void unk_04() = 0;
+	virtual void unk_05() = 0;
+	virtual void unk_06() = 0;
+	virtual void unk_07() = 0;
+	virtual void unk_08() = 0;
+	virtual void unk_09() = 0;
+	virtual void unk_10() = 0;
 };
 
 typedef void (*EntityResourceManifestCreationCallback_t)(IEntityResourceManifest *, void *);
