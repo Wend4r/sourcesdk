f01efd1ad64f7c329a044c404394f68aa033e231
Wend4r <47463683+Wend4r@users.noreply.github.com>
2026-03-17 14:21:46 +0300

    Remove `IEntity2Networkables::CreateEntity2Networkable` method

--- public/entity2/entitynetwork.h
+++ public/entity2/entitynetwork.h
@@ -46,7 +46,6 @@ public:
 
 	virtual bool GetEntity2Networkable( CEntityIndex nEntryIndex, Entity2Networkable_t *info ) = 0;
 	virtual CUtlMap< int, Entity2Networkable_t > &GetEntity2Networkables( void ) const = 0;
-	virtual void CreateEntity2Networkable() = 0;
 	virtual void Unk01() = 0;
 	virtual void Unk02() = 0;
 	virtual ServerClass **GetClassList() = 0;
@@ -59,10 +58,10 @@ public:
 class CEntity2NetworkClasses : public IEntity2Networkables, public IEntityListener
 {
 public:
-	ServerClass **m_ppClassList;
+	ServerClass *m_pClassList;
 	void *m_pPad[2];
 	CUtlMap< int, Entity2Networkable_t > m_NetworkedEntities2;
 };
-
+static_assert( sizeof( CEntity2NetworkClasses ) == 72 );
 
 #endif // ENTITYNETWORK_H
