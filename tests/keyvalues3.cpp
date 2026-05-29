c6479bbba5b1660bf5a0420aebec32c7e6cee7d4
Wend4r <47463683+Wend4r@users.noreply.github.com>
2026-05-29 16:48:32 +0300

    Add KeyValues3 load coverage from test data

--- tests/keyvalues3.cpp
+++ tests/keyvalues3.cpp
@@ -1,8 +1,141 @@
 #include "common/assert.h"
 #include "common/macros.h"
 
+#include <tier0/keyvalues3.h>
 #include <tier0/strtools.h>
+#include <tier0/utlbuffer.h>
+#include <tier0/utlstring.h>
 #include <tier1/keyvalues3.h>
+
+#include <cmath>
+#include <fstream>
+#include <iterator>
+#include <string>
+
+static CUtlString ReadKeyValues3TestFile( const char *pFilename )
+{
+	std::ifstream file( pFilename, std::ios::in | std::ios::binary );
+
+	TEST_TRUE( file.is_open() );
+
+	std::string sText( ( std::istreambuf_iterator< char >( file ) ), std::istreambuf_iterator< char >() );
+
+	return CUtlString( sText.c_str() );
+}
+
+template < int N >
+static KeyValues3 *FindRequiredMember( KeyValues3 &kv, const char (&pName)[N] )
+{
+	KeyValues3 *pMember = kv.FindMember( pName );
+	TEST_NOT_NULL( pMember );
+	return pMember;
+}
+
+static void TestFloatClose( float flValue, float flExpected )
+{
+	TEST_TRUE( std::fabs( flValue - flExpected ) < 0.001f );
+}
+
+static void TestDoubleClose( double flValue, double flExpected )
+{
+	TEST_TRUE( std::fabs( flValue - flExpected ) < 0.001 );
+}
+
+static void ValidateKV3TypeExMembers( KeyValues3 &kv, bool bExpectBinaryBlob )
+{
+	KeyValues3 *pNull = FindRequiredMember( kv, "null_member" );
+
+	TEST_EQ( pNull->GetTypeEx(), KV3_TYPEEX_NULL );
+	TEST_TRUE( pNull->IsNull() );
+
+	KeyValues3 *pBool = FindRequiredMember( kv, "bool_member" );
+
+	TEST_EQ( pBool->GetTypeEx(), KV3_TYPEEX_BOOL );
+	TEST_TRUE( pBool->GetBool() );
+
+	KeyValues3 *pInt = FindRequiredMember( kv, "int_member" );
+
+	TEST_EQ( pInt->GetTypeEx(), KV3_TYPEEX_INT );
+	TEST_EQ( pInt->GetInt(), -42 );
+
+	KeyValues3 *pUInt = FindRequiredMember( kv, "uint_member" );
+
+	TEST_EQ( pUInt->GetTypeEx(), KV3_TYPEEX_UINT );
+	TEST_EQ( pUInt->GetUInt64(), 18446744073709551615ull );
+
+	KeyValues3 *pDouble = FindRequiredMember( kv, "double_member" );
+
+	TEST_EQ( pDouble->GetTypeEx(), KV3_TYPEEX_DOUBLE );
+	TestDoubleClose( pDouble->GetDouble(), 12.5 );
+
+	KeyValues3 *pStringShort = FindRequiredMember( kv, "string_short_member" );
+
+	TEST_EQ( pStringShort->GetTypeEx(), KV3_TYPEEX_STRING_SHORT );
+	TEST_EQ( V_strcmp( pStringShort->GetString(), "short" ), 0 );
+
+	KeyValues3 *pString = FindRequiredMember( kv, "string_member" );
+
+	TEST_EQ( pString->GetTypeEx(), KV3_TYPEEX_STRING );
+	TEST_EQ( V_strcmp( pString->GetString(), "this string is longer than seven bytes" ), 0 );
+
+	if ( bExpectBinaryBlob )
+	{
+		KeyValues3 *pBlob = FindRequiredMember( kv, "binary_blob_member" );
+
+		TEST_EQ( pBlob->GetTypeEx(), KV3_TYPEEX_BINARY_BLOB );
+		TEST_EQ( pBlob->GetBinaryBlobSize(), 4 );
+		TEST_EQ( pBlob->GetBinaryBlob()[0], 0x00 );
+		TEST_EQ( pBlob->GetBinaryBlob()[1], 0x7F );
+		TEST_EQ( pBlob->GetBinaryBlob()[2], 0xA5 );
+		TEST_EQ( pBlob->GetBinaryBlob()[3], 0xFF );
+	}
+
+	KeyValues3 *pArray = FindRequiredMember( kv, "array_member" );
+
+	TEST_EQ( pArray->GetTypeEx(), KV3_TYPEEX_ARRAY );
+	TEST_EQ( pArray->GetArrayElementCount(), 7 );
+	TEST_TRUE( pArray->GetArrayElement( 0 )->IsNull() );
+	TEST_TRUE( pArray->GetArrayElement( 1 )->GetBool() == false );
+	TEST_EQ( pArray->GetArrayElement( 2 )->GetInt(), -1 );
+	TestDoubleClose( pArray->GetArrayElement( 4 )->GetDouble(), 3.5 );
+	TEST_EQ( V_strcmp( pArray->GetArrayElement( 5 )->GetString(), "array" ), 0 );
+	TEST_TRUE( pArray->GetArrayElement( 6 )->GetMemberBool( "nested" ) );
+
+	KeyValues3 *pFloatArray = FindRequiredMember( kv, "array_float_member" );
+
+	TEST_EQ( pFloatArray->GetTypeEx(), KV3_TYPEEX_ARRAY );
+	TEST_EQ( pFloatArray->GetArrayElementCount(), 3 );
+	TestDoubleClose( pFloatArray->GetArrayElement( 0 )->GetDouble(), 1.25 );
+
+	KeyValues3 *pIntArray = FindRequiredMember( kv, "array_int_member" );
+
+	TEST_EQ( pIntArray->GetTypeEx(), KV3_TYPEEX_ARRAY );
+	TEST_EQ( pIntArray->GetArrayElementCount(), 3 );
+	TEST_EQ( pIntArray->GetArrayElement( 0 )->GetInt(), 100000 );
+
+	KeyValues3 *pTable = FindRequiredMember( kv, "table_member" );
+
+	TEST_EQ( pTable->GetTypeEx(), KV3_TYPEEX_TABLE );
+	TEST_EQ( pTable->GetMemberInt( "child_int" ), 7 );
+	TEST_EQ( V_strcmp( pTable->FindMember( "child_object" )->GetMemberString( "name" ), "nested" ), 0 );
+	TEST_EQ( pTable->FindMember( "child_array" )->GetArrayElementCount(), 2 );
+}
+
+static void ValidateKV1TranslatedMembers( KeyValues3 &kv )
+{
+	TEST_TRUE( kv.GetMemberBool( "bool_member" ) );
+	TEST_EQ( kv.GetMemberInt( "int_member" ), -42 );
+	TEST_EQ( kv.GetMemberUInt64( "uint_member" ), 18446744073709551615ull );
+	TestDoubleClose( kv.GetMemberDouble( "double_member" ), 12.5 );
+	TEST_EQ( V_strcmp( kv.GetMemberString( "string_short_member" ), "short" ), 0 );
+	TEST_EQ( V_strcmp( kv.GetMemberString( "string_member" ), "this string is longer than seven bytes" ), 0 );
+
+	KeyValues3 *pTable = FindRequiredMember( kv, "table_member" );
+
+	TEST_TRUE( pTable->IsTable() );
+	TEST_EQ( pTable->GetMemberInt( "child_int" ), 7 );
+	TEST_EQ( V_strcmp( pTable->FindMember( "child_object" )->GetMemberString( "name" ), "nested" ), 0 );
+}
 
 REGISTER_NAMED_TEST( "KeyValues3.Empty", KeyValues3_Empty )
 {
@@ -83,4 +216,119 @@ REGISTER_NAMED_TEST( "KeyValues3.Table", KeyValues3_Table )
 
 	TEST_TRUE( kv.RemoveMember( "answer" ) );
 	TEST_EQ( kv.GetMemberInt( "answer", -1 ), -1 );
+}
+
+REGISTER_NAMED_TEST( "KeyValues3.LoadText.KV1", KeyValues3_LoadText_KV1 )
+{
+	KeyValues3 kv;
+	CUtlString sError;
+	const CUtlString sText = ReadKeyValues3TestFile( SOURCESDK_KEYVALUES3_DATA_DIR "/value.kv" );
+
+	TEST_TRUE( LoadKV3FromKV1Text( &kv, &sError, sText.Get(), KV1TEXT_ESC_BEHAVIOR_UNK1, "value.kv", false ) );
+	TEST_TRUE( kv.IsTable() );
+	TEST_EQ( kv.GetMemberInt( "answer" ), 41 );
+	TEST_EQ( V_strcmp( kv.GetMemberString( "name" ), "kv" ), 0 );
+	ValidateKV1TranslatedMembers( kv );
+}
+
+REGISTER_NAMED_TEST( "KeyValues3.LoadText.KV3", KeyValues3_LoadText_KV3 )
+{
+	KeyValues3 kv;
+	CUtlString sError;
+	const CUtlString sText = ReadKeyValues3TestFile( SOURCESDK_KEYVALUES3_DATA_DIR "/value.kv3" );
+
+	TEST_TRUE( LoadKV3( &kv, &sError, sText.Get(), g_KV3Format_Generic, "value.kv3" ) );
+	TEST_TRUE( kv.IsTable() );
+	TEST_EQ( kv.GetMemberInt( "answer" ), 42 );
+	TEST_EQ( V_strcmp( kv.GetMemberString( "name" ), "kv3" ), 0 );
+	ValidateKV3TypeExMembers( kv, true );
+}
+
+REGISTER_NAMED_TEST( "KeyValues3.LoadText.JSON", KeyValues3_LoadText_JSON )
+{
+	KeyValues3 kv;
+	CUtlString sError;
+	const CUtlString sText = ReadKeyValues3TestFile( SOURCESDK_KEYVALUES3_DATA_DIR "/value.json" );
+
+	TEST_TRUE( LoadKV3FromJSON( &kv, &sError, sText.Get(), "value.json" ) );
+	TEST_TRUE( kv.IsTable() );
+	TEST_EQ( kv.GetMemberInt( "answer" ), 43 );
+	TEST_EQ( V_strcmp( kv.GetMemberString( "name" ), "json" ), 0 );
+	ValidateKV3TypeExMembers( kv, false );
+}
+
+REGISTER_NAMED_TEST( "KeyValues3.LoadText.TypeExMembers", KeyValues3_LoadText_TypeExMembers )
+{
+	KeyValues3 kv;
+	CUtlString sError;
+	const CUtlString sText = ReadKeyValues3TestFile( SOURCESDK_KEYVALUES3_DATA_DIR "/typeex.kv3" );
+
+	TEST_TRUE( LoadKV3( &kv, &sError, sText.Get(), g_KV3Format_Generic, "typeex.kv3" ) );
+	TEST_TRUE( kv.IsTable() );
+	ValidateKV3TypeExMembers( kv, true );
+}
+
+REGISTER_NAMED_TEST( "KeyValues3.LoadBinary.ArrayMembers", KeyValues3_LoadBinary_ArrayMembers )
+{
+	KeyValues3 source;
+	source.SetToEmptyTable();
+	source.SetMemberVector( "vector_member", Vector( 1.25f, 2.5f, 3.75f ) );
+	source.SetMemberColor( "color_member", Color( 1, 2, 3, 4 ) );
+
+	CUtlString sError;
+	CUtlBuffer buffer( 0, 0, CUtlBuffer::NONE );
+
+	TEST_TRUE( SaveKV3( g_KV3Encoding_Binary, g_KV3Format_Generic, &source, &sError, &buffer ) );
+	buffer.SeekGet( CUtlBuffer::SEEK_HEAD, 0 );
+
+	KeyValues3 loaded;
+
+	TEST_TRUE( LoadKV3( &loaded, &sError, &buffer, g_KV3Format_Generic, "typeex-binary.kv3" ) );
+	TEST_TRUE( loaded.IsTable() );
+
+	KeyValues3 *pVector = FindRequiredMember( loaded, "vector_member" );
+
+	TEST_TRUE( pVector->IsArray() );
+	TEST_EQ( pVector->GetArrayElementCount(), 3 );
+	Vector vec = pVector->GetVector();
+	TestFloatClose( vec.x, 1.25f );
+	TestFloatClose( vec.y, 2.5f );
+	TestFloatClose( vec.z, 3.75f );
+
+	KeyValues3 *pColor = FindRequiredMember( loaded, "color_member" );
+
+	TEST_TRUE( pColor->IsArray() );
+	TEST_EQ( pColor->GetArrayElementCount(), 4 );
+	Color color = pColor->GetColor();
+	TEST_EQ( color.r(), 1 );
+	TEST_EQ( color.g(), 2 );
+	TEST_EQ( color.b(), 3 );
+	TEST_EQ( color.a(), 4 );
+}
+
+REGISTER_NAMED_TEST( "KeyValues3.LoadBuffer.KV3OrKV1", KeyValues3_LoadBuffer_KV3OrKV1 )
+{
+	KeyValues3 kv;
+	CUtlString sError;
+	const CUtlString sText = ReadKeyValues3TestFile( SOURCESDK_KEYVALUES3_DATA_DIR "/buffer.kv3" );
+	CUtlBuffer buffer( sText.Get(), sText.Length(), ( CUtlBuffer::BufferFlags_t )( CUtlBuffer::TEXT_BUFFER | CUtlBuffer::READ_ONLY ) );
+
+	TEST_TRUE( LoadKV3FromKV3OrKV1( &kv, &sError, &buffer, g_KV3Format_Generic, "buffer.kv3" ) );
+	TEST_TRUE( kv.IsTable() );
+	TEST_EQ( kv.GetMemberInt( "answer" ), 44 );
+	TEST_EQ( V_strcmp( kv.GetMemberString( "name" ), "buffer" ), 0 );
+	ValidateKV3TypeExMembers( kv, true );
+}
+
+REGISTER_NAMED_TEST( "KeyValues3.LoadText.NoHeader", KeyValues3_LoadText_NoHeader )
+{
+	KeyValues3 kv;
+	CUtlString sError;
+	const CUtlString sText = ReadKeyValues3TestFile( SOURCESDK_KEYVALUES3_DATA_DIR "/no_header.kv3" );
+
+	TEST_TRUE( LoadKV3Text_NoHeader( &kv, &sError, sText.Get(), g_KV3Format_Generic, "no-header.kv3" ) );
+	TEST_TRUE( kv.IsTable() );
+	TEST_EQ( kv.GetMemberInt( "answer" ), 45 );
+	TEST_EQ( V_strcmp( kv.FindMember( "nested" )->GetMemberString( "value" ), "no-header" ), 0 );
+	ValidateKV3TypeExMembers( kv, true );
 }
