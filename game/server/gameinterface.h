5f8b0ca6a27a6317e17e42565c8dc7de05f80ffa
komashchenko <komashchenko@users.noreply.github.com>
2026-09-11 15:45:07 +0300

    fix: correct `SvcUserMessageHandlerInfo_t` layout on linux

--- game/server/gameinterface.h
+++ game/server/gameinterface.h
@@ -18,12 +18,27 @@ class CSource2GameClients : public CGameAppSystem< ISource2GameClients >
 public:
 	struct SvcUserMessageHandlerInfo_t;
 
-	typedef void ( *SvcUserMessageHandlerFn_t )( CSource2GameClients *pThis, uint32 nSize, const void *pBuffer, SvcUserMessageHandlerInfo_t *pInfo, CBasePlayerController *pPlayer );
-
+	typedef void ( *SvcUserMessageHandlerFn_t )( CSource2GameClients *pSource2GameClients, uint32 nSize, const void *pBuffer, SvcUserMessageHandlerInfo_t *pInfo, CBasePlayerController *pPlayer );
 
 	struct SvcUserMessageHandlerInfo_t
 	{
+		SvcUserMessageHandlerInfo_t() : SvcUserMessageHandlerInfo_t( nullptr )
+		{
+		}
+
+		explicit SvcUserMessageHandlerInfo_t( SvcUserMessageHandlerFn_t pfnHandler ) : m_pfnHandler( pfnHandler )
+#ifndef _WIN32
+			, m_nThisAdjust( 0 )
+#endif
+		{
+		}
+
+		// Native declaration: typedef void ( CSource2GameClients::*SvcUserMessageHandlerFn_t )( uint32 nSize, const void *pBuffer, SvcUserMessageHandlerInfo_t *pInfo, CBasePlayerController *pPlayer );
 		SvcUserMessageHandlerFn_t m_pfnHandler;
+#ifndef _WIN32
+		// On Linux, m_nThisAdjust completes the native member-function pointer.
+		ptrdiff_t m_nThisAdjust;
+#endif
 		CUtlVector< CUtlAbstractDelegate > m_Delegates;
 	};
 
